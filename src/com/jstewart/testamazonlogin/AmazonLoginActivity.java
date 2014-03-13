ceeaaa31e09d91ff977889c20a34bdd1b47ed700
Jeremy Stewart <jlstewart379@gmail.com>
2014-03-12 23:08:06 -0400

    AuthorizationManager finally recognizing api_key

--- src/com/jstewart/testamazonlogin/AmazonLoginActivity.java
+++ src/com/jstewart/testamazonlogin/AmazonLoginActivity.java
@@ -1,19 +1,34 @@
 package com.jstewart.testamazonlogin;
 
+import com.amazon.identity.auth.device.authorization.api.AmazonAuthorizationManager;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 public class AmazonLoginActivity extends FragmentActivity {
-	
-	private AuthorizationManager mAuthorizationManager; 
-	
-	
+
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
+		
+		AmazonAuthorizationManager mAuthManager = new AmazonAuthorizationManager(this,
+				Bundle.EMPTY);
+		
+		setContentView(R.layout.activity_amazon_login);
+
+		FragmentManager fm = getFragmentManager();
+		Fragment loginFragment = fm
+				.findFragmentById(R.id.login_fragment_container);
+
+		if (loginFragment == null) {
+			loginFragment = new LoginFragment();
+		}
+
+		fm.beginTransaction().add(R.id.login_fragment_container, loginFragment)
+				.commit();
+
 	}
-
-
-	
 
 }
--- src/com/jstewart/testamazonlogin/LoginFragment.java
+++ src/com/jstewart/testamazonlogin/LoginFragment.java
@@ -2,23 +2,105 @@ package com.jstewart.testamazonlogin;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.amazon.identity.auth.device.AuthError;
+import com.amazon.identity.auth.device.authorization.api.AmazonAuthorizationManager;
+import com.amazon.identity.auth.device.authorization.api.AuthorizationListener;
+import com.amazon.identity.auth.device.authorization.api.AuthzConstants;
+import com.amazon.identity.auth.device.shared.APIListener;
 
 public class LoginFragment extends Fragment {
 
 	private ImageButton loginButton;
+	private AmazonAuthorizationManager mAuthManager;
+	private String[] AUTH_SCOPES = new String[] { "profile" };
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+
+
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		View view = inflater.inflate(R.layout.fragment_login, container);
-		loginButton.setOnClickListener(new ); 
+		View view = inflater.inflate(R.layout.fragment_login, null);
+		
+		loginButton = (ImageButton) view.findViewById(
+				R.id.login_with_amazon_button);
+//		loginButton.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//
+//				mAuthManager.authorize(AUTH_SCOPES, Bundle.EMPTY,
+//						new AuthListener());
+//
+//			}
+//		});
 
 		return view;
+	}
+
+	private class AuthListener implements AuthorizationListener {
+
+		@Override
+		public void onError(AuthError error) {
+			Log.e("Auth Listener - onError", "Error getting profile data: "
+					+ error.getMessage());
+			Toast.makeText(getActivity(), "Error getting profile data",
+					Toast.LENGTH_SHORT).show();
+
+		}
+
+		@Override
+		public void onSuccess(Bundle response) {
+			mAuthManager.getProfile(new ProfileListener());
+
+		}
+
+		@Override
+		public void onCancel(Bundle response) {
+			Toast.makeText(getActivity(), "Authorization canceled",
+					Toast.LENGTH_SHORT).show();
+		}
+
+	}
+
+	private class ProfileListener implements APIListener {
+
+		@Override
+		public void onError(AuthError response) {
+			Log.e("Profile Listener - onError", "Error getting profile data: "
+					+ response.getMessage());
+			Toast.makeText(getActivity(), "Error getting profile data",
+					Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onSuccess(Bundle response) {
+			Bundle profileData = response
+					.getBundle(AuthzConstants.BUNDLE_KEY.PROFILE.val);
+			String id = profileData
+					.getString(AuthzConstants.PROFILE_KEY.USER_ID.val);
+			String name = profileData
+					.getString(AuthzConstants.PROFILE_KEY.NAME.val);
+			String email = profileData
+					.getString(AuthzConstants.PROFILE_KEY.EMAIL.val);
+
+			Log.e("Profile Listener - onSuccess", "We have bundle data. Id: "
+					+ id + " Name: " + name + " Email: " + email);
+
+		}
 	}
 
 }
